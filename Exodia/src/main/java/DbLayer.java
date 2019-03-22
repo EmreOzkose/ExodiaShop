40bf5b6ef371092f0669a0b0afaf242231c79f3d
MTUSTA <mehmettahausta@hotmail.com>
2019-03-23 00:10:50 +0300

    Update DbLayer.java

--- Exodia/src/main/java/DbLayer.java
+++ Exodia/src/main/java/DbLayer.java
@@ -10,31 +10,43 @@ public class DbLayer {
     private ResultSet myRs = null;
     
     public void connect(String user,String pass) throws SQLException{
-            try {
-                // 1. Get a connection to database
-                myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/exodiadb?useUnicode=true&useLegacyDatetimeCode=false&serverTimezone=Turkey", user, pass);
-            } catch (Exception exc) {
-                exc.printStackTrace();
-            }
+        try {
+        // 1. Get a connection to database
+            myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/exodiadb?useUnicode=true&useLegacyDatetimeCode=false&serverTimezone=Turkey", user, pass);
+        } catch (Exception exc) {
+            exc.printStackTrace();
+        }
     }
-    public void Command(DbLayer DB,String Commd) throws SQLException{
-    // 2. Create a statement
-    DB.myStmt = DB.myConn.createStatement();
+    public void Command(String Commd) throws SQLException{
+        // 2. Create a statement
+        myStmt = myConn.createStatement();
 
-    // 3. Execute SQL query
-    DB.myRs = DB.myStmt.executeQuery(Commd);
- 
-    // 4. Process the result set
-    while (DB.myRs.next()){
-    System.out.println(DB.myRs.getString("Name") + ", " + DB.myRs.getString("Surname"));
-    }       
+        // 3. Execute SQL query
+        myRs = myStmt.executeQuery(Commd);
+
+        // 4. Process the result set
+        while (myRs.next()){
+        System.out.println(myRs.getString("Name") + ", " + myRs.getString("Surname"));
+        }       
+    }
+    public void Close() throws SQLException{
+        if (myRs != null) {
+            myRs.close();
+        }
+        if (myStmt != null) {
+            myStmt.close();
+        }
+        if (myConn != null) {
+        myConn.close();
+        }
     }
     
     public static void main(String[] args) throws SQLException {
     DbLayer deneme = new DbLayer();
     deneme.connect("root","aezakmi1997");
     /*bura düzenlenmesi gerekiyor ic kodlar dahil*/
-    deneme.Command(deneme,"select * from customer");
+    deneme.Command("select * from customer");
+    deneme.Close();
     }
     
 }
