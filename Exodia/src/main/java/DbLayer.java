119daf6cdf666ee9af4e3e1dd62ece556acabdd5
MTUSTA <mehmettahausta@hotmail.com>
2019-03-22 23:50:34 +0300

    Update DbLayer.java

--- Exodia/src/main/java/DbLayer.java
+++ Exodia/src/main/java/DbLayer.java
@@ -1,14 +1,4 @@
-
 import java.sql.*;
-
-
-
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
  *
@@ -19,41 +9,32 @@ public class DbLayer {
     private Statement myStmt = null;
     private ResultSet myRs = null;
     
-    public Connection connect(String user,String pass) throws SQLException{
+    public void connect(String user,String pass) throws SQLException{
             try {
                 // 1. Get a connection to database
                 myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/exodiadb?useUnicode=true&useLegacyDatetimeCode=false&serverTimezone=Turkey", user, pass);
-
-                // 2. Create a statement
-                myStmt = myConn.createStatement();
-
-                // 3. Execute SQL query
-                myRs = myStmt.executeQuery("select * from customer");
-
-                // 4. Process the result set
-                while (myRs.next()) {
-                    System.out.println(myRs.getString("Name") + ", " + myRs.getString("Surname"));
-                }
-
             } catch (Exception exc) {
                 exc.printStackTrace();
-            } finally {
-                if (myRs != null) {
-                    myRs.close();
-                }
-                if (myStmt != null) {
-                    myStmt.close();
-                }
-
-                if (myConn != null) {
-                    myConn.close();
-                }
             }
-            return myConn;    
     }
+    public void Command(DbLayer DB,String Commd) throws SQLException{
+    // 2. Create a statement
+    DB.myStmt = DB.myConn.createStatement();
+
+    // 3. Execute SQL query
+    DB.myRs = DB.myStmt.executeQuery(Commd);
+ 
+    // 4. Process the result set
+    while (DB.myRs.next()){
+    System.out.println(DB.myRs.getString("Name") + ", " + DB.myRs.getString("Surname"));
+    }       
+    }
+    
     public static void main(String[] args) throws SQLException {
     DbLayer deneme = new DbLayer();
     deneme.connect("root","aezakmi1997");
+    deneme.Command(deneme,"select * from customer");
+    
     }
     
 }
